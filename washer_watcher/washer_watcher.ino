3859678a60446499675d7bda2239cd3c77687c43
garamizo <garamizo@mtu.edu>
2017-10-01 13:56:07 -0400

    replace to cheaper imu: mpu6050

--- washer_watcher/washer_watcher.ino
+++ washer_watcher/washer_watcher.ino
@@ -1,29 +1,11 @@
-/*
-  WriteVoltage
-  
-  Reads an analog voltage from pin 0, and writes it to a channel on ThingSpeak every 20 seconds.
-  
-  ThingSpeak ( https://www.thingspeak.com ) is an analytic IoT platform service that allows you to aggregate, visualize and 
-  analyze live data streams in the cloud.
-  
-  Copyright 2017, The MathWorks, Inc.
-  
-  Documentation for the ThingSpeak Communication Library for Arduino is in the extras/documentation folder where the library was installed.
-  See the accompaning licence file for licensing information.
-*/
 
-#include "ThingSpeak.h"
+#define MPU_addr 0x68
+#define TO_CLOUD
+
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
 
-// ***********************************************************************************************************
-// This example selects the correct library to use based on the board selected under the Tools menu in the IDE.
-// Yun, Ethernet shield, WiFi101 shield, esp8266, and MXR1000 are all supported.
-// With Yun, the default is that you're using the Ethernet connection.
-// If you're using a wi-fi 101 or ethernet shield (http://www.arduino.cc/en/Main/ArduinoWiFiShield), uncomment the corresponding line below
-// ***********************************************************************************************************
+#ifdef TO_CLOUD
+#include "ThingSpeak.h"
 
 //#define USE_WIFI101_SHIELD
 //#define USE_ETHERNET_SHIELD
@@ -53,42 +35,15 @@
   #endif
 #endif
 
-#define BNO055_SAMPLERATE_DELAY_MS (10)
-
-Adafruit_BNO055 bno = Adafruit_BNO055();
-
-#ifdef ARDUINO_ARCH_AVR
-  // On Arduino:  0 - 1023 maps to 0 - 5 volts
-  #define VOLTAGE_MAX 5.0
-  #define VOLTAGE_MAXCOUNTS 1023.0
-#elif ARDUINO_SAMD_MKR1000
-  // On MKR1000:  0 - 1023 maps to 0 - 3.3 volts
-  #define VOLTAGE_MAX 3.3
-  #define VOLTAGE_MAXCOUNTS 1023.0
-#elif ARDUINO_SAM_DUE
-  // On Due:  0 - 1023 maps to 0 - 3.3 volts
-  #define VOLTAGE_MAX 3.3
-  #define VOLTAGE_MAXCOUNTS 1023.0  
-#elif ARDUINO_ARCH_ESP8266
-  // On ESP8266:  0 - 1023 maps to 0 - 1 volts
-  #define VOLTAGE_MAX 1.0
-  #define VOLTAGE_MAXCOUNTS 1023.0
-#endif
-
-/*
-  *****************************************************************************************
-  **** Visit https://www.thingspeak.com to sign up for a free account and create
-  **** a channel.  The video tutorial http://community.thingspeak.com/tutorials/thingspeak-channels/ 
-  **** has more information. You need to change this to your channel, and your write API key
-  **** IF YOU SHARE YOUR CODE WITH OTHERS, MAKE SURE YOU REMOVE YOUR WRITE API KEY!!
-  *****************************************************************************************/
 unsigned long myChannelNumber = 315859;
 const char * myWriteAPIKey = "AEHJ15QUUHUQADLU";
+#endif
 
 void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
-  
+
+  #ifdef TO_CLOUD
   #ifdef ARDUINO_AVR_YUN
     Bridge.begin();
   #else   
@@ -100,27 +55,30 @@ void setup() {
   #endif
 
   ThingSpeak.begin(client);
+  #endif
 
-
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  
   Serial.begin(9600);
-  Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
-
-  /* Initialise the sensor */
-  if(!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
 
   delay(1000);
 
-  bno.setExtCrystalUse(true);
+}
 
-  for(int k = 0; k < 100; k++) { // fix initial reading problem
-    imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-    delay(10);
-  }
+float read_acceleration() {
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 6, true);  // request a total of 14 registers
+  
+  int16_t AcX = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  int16_t AcY = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  int16_t AcZ = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  return( 9.81 * sqrt(AcX*AcX + AcY*AcY + AcZ*AcZ) / 2048.0 );
 }
 
 
@@ -133,18 +91,21 @@ void loop() {
 
     float vmax = 0, vmin = 1e3;
     for(int k = 0; k < 100; k++) {
-      imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-      float acc_abs = sqrt(acc.x()*acc.x() + acc.y()*acc.y() + acc.z()*acc.z());
+      float acc_abs = read_acceleration(); // g/sec^2
 
       vmax = acc_abs > vmax ? acc_abs : vmax;
       vmin = acc_abs < vmin ? acc_abs : vmin;
       
-      delay(BNO055_SAMPLERATE_DELAY_MS);
+      delay(10);
     }
     
     // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
     // pieces of information in a channel.  Here, we write to field 1.
+    #ifdef TO_CLOUD
     ThingSpeak.writeField(myChannelNumber, 1, vmax-vmin, myWriteAPIKey);
+    #else
+    Serial.println(vmax-vmin);
+    #endif
   }
 
   if(millis() > t2) {
